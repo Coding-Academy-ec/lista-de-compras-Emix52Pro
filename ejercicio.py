117f793a4e53f676dd7ecf4079adc85fa8eccac5
Gabriel Garcia <ggarcia7@utmachala.edu.ec>
2024-06-15 11:50:42 -0500

    Test para comprobar si esta bien

--- ejercicio.py
+++ ejercicio.py
@@ -1,12 +1,11 @@
 # Ingrese una lista de compras: manzanas, plátanos, leche, pan
 # Los productos en la lista de compras son: ['manzanas', 'plátanos', 'leche', 'pan']
 
-compras = input("Ingrese una lista de compras: ")
-productos = compras.split(", ")
-print(f"Los productos en la lista de compras son: {productos}")
-
 def convertir_lista_a_tupla(lista):
     return tuple(lista)  # Convertir la lista a tupla
 
-productos_tupla = convertir_lista_a_tupla(productos)
-
+if __name__ == "__main__":
+    compras = input("Ingrese una lista de compras: ")
+    productos = compras.split(", ")
+    print(f"Los productos en la lista de compras son: {productos}")
+    productos_tupla = convertir_lista_a_tupla(productos)
